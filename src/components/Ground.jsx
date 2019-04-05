4a74a07fcad76cc19d0e4efe9932689ef970f00d
Lalit Thapa <lalit.thapa@ekbana.info>
2019-04-05 14:19:05 +0545

    create ground component

---
+++ src/components/Ground.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Ground = () => {
+  const style = {
+    fill: '#59a941',
+  }
+  const division = {
+    stroke: '#458232',
+    strokeWidth: '3px',
+  }
+
+  const width = 5000
+
+  return (
+    <g id='ground'>
+      <rect
+        id='ground-2'
+        data-name='Ground'
+        style={style}
+        x={width / -2}
+        y={0}
+        width={width}
+        height={100}
+      />
+      <line
+        x1={width / -2}
+        y1={0}
+        x2={width / 2}
+        y2={0}
+        style={division}
+      />
+    </g>
+  )
+}
+
+export default Ground
