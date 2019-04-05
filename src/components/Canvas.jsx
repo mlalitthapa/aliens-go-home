98d5a29f0b2b2e3b11b1ceb07e55cacb2fa02754
Lalit Thapa <lalit.thapa@ekbana.info>
2019-04-05 14:10:47 +0545

    add sky in canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Sky from './Sky'
+
 const Canvas = () => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight]
   return (
@@ -8,6 +10,7 @@ const Canvas = () => {
       preserveAspectRatio='xMaxYMax none'
       viewBox={viewBox}
     >
+      <Sky/>
       <circle cx={0} cy={0} r={50}/>
     </svg>
   )
