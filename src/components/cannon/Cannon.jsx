f62a90f5aac1335ab7c53db1a367b49fb859cef3
Lalit Thapa <lalit.thapa@ekbana.info>
2019-04-05 16:16:53 +0545

    create cannon ball and add it to canvas

--- src/components/cannon/Cannon.jsx
+++ src/components/cannon/Cannon.jsx
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 
 import CannonPipe from './CannonPipe'
 import CannonBase from './CannonBase'
+import CannonBall from './CannonBall'
 
 const Cannon = (props) => {
   return (
     <g id='cannon'>
       <CannonPipe rotation={props.rotation}/>
       <CannonBase/>
+      <CannonBall position={{x: 0, y: -100}}/>
     </g>
   )
 }
