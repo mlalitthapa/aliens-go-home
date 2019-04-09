29230d9cbef736a367d05bbea5ba364c6b4bcc1e
Lalit Thapa <mlalitthapa@gmail.com>
2019-04-09 21:45:39 +0545

    add score component in canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import Sky from './Sky'
 import Ground from './Ground'
 import Cannon from './cannon/Cannon'
+import Score from './Score'
 
 const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight]
@@ -14,9 +15,15 @@ const Canvas = (props) => {
       onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
       <Sky/>
       <Ground/>
       <Cannon rotation={props.angle}/>
+      <Score score={40}/>
     </svg>
   )
 }
