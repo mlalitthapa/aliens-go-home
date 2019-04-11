96c0027d5782976880e973670973098d173a337e
Lalit Thapa <lalit.thapa@ekbana.info>
2019-04-11 15:04:59 +0545

    animate flying objects

--- src/components/ufo/UFO.jsx
+++ src/components/ufo/UFO.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled, { keyframes } from 'styled-components'
 import UFOTop from './UFOTop'
 import UFOBase from './UFOBase'
+import { GAME_HEIGHT } from '../../utils/constants'
+
+const moveVertically = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(${GAME_HEIGHT}px);
+  }
+`
+
+const Move = styled.g`
+  animation: ${moveVertically} 4s linear;
+`
 
 const UFO = props => (
-  <g>
+  <Move>
     <UFOTop position={props.position}/>
     <UFOBase position={props.position}/>
-  </g>
+  </Move>
 )
 
 UFO.propTypes = {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,9 @@
 export const SKY_AND_GROUND_WIDTH = 5000
 export const GAME_WIDTH = 800
+export const GAME_HEIGHT = 1200
 
 // constants for flying objects
 export const INTERVAL = 1000
 export const MAX_OBJECTS = 4
-export const STARTER_Y_AXIS = -1000
+export const STARTER_Y_AXIS = -1100
 export const STARTER_POSITIONS = [-300, -150, 150, 300]
