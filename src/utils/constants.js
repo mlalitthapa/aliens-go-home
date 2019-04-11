6930944da5b15911ce712e11e9e97175b2a8337f
Lalit Thapa <lalit.thapa@ekbana.info>
2019-04-11 13:46:34 +0545

    add constants for flying objects

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,8 @@
 export const SKY_AND_GROUND_WIDTH = 5000
 export const GAME_WIDTH = 800
+
+// constants for flying objects
+export const INTERVAL = 1000
+export const MAX_OBJECTS = 4
+export const STARTER_Y_AXIS = -1000
+export const STARTER_POSITIONS = [-300, -150, 150, 300]
